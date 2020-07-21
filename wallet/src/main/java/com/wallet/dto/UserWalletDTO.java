134b1ff7f0b6cbf8d3a6d1026da4ec2552e06687
mathmich <mathmich@gmail.com>
2020-07-21 05:35:44 -0300

    UserWallet Repository

--- wallet/src/main/java/com/wallet/dto/UserWalletDTO.java
+++ wallet/src/main/java/com/wallet/dto/UserWalletDTO.java
@@ -1,5 +1,15 @@
 package com.wallet.dto;
 
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
 public class UserWalletDTO {
 
+		private Long id;  // está tudo como Long pois está trafegando apenas o ID
+		@NotNull(message = "Informe o ID do usuário")
+		private Long users;
+		@NotNull(message = "Informe o ID da carteira")
+		private Long wallet;
 }
---
+++ wallet/src/main/java/com/wallet/repository/UserWalletRepository.java
@@ -0,0 +1,7 @@
+package com.wallet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserWalletRepository extends JpaRepository<UserWallet, Long> {
+
+}
